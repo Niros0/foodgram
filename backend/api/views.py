3fd287f80d95f5a8796277f0f88aad09495d318a
Niros0 <168647877+Niros0@users.noreply.github.com>
2025-01-14 09:16:15 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework.views import APIView
 from rest_framework import status
